de08151f4c30bce351dc0032396bef83d9f45f6d
Taconomicon <DylanMinkt@gmail.com>
2016-04-10 11:03:36 -0400

    Converted HTML to Haml.
    Bitcamp 2016

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ActiveRecord::Base
+	validates :title, presence:true, length: {minimum: 1, maximum: 67}
+	validates :content, presence:true, length: {minimum: 1}
 	belongs_to :user
 	has_many :comments
 end
